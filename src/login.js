18ea91259da8a445f952d525a22a10b140c0fb48
nishant-dongare <nishantdongare@outlook.com>
2022-11-06 14:37:49 +0530

    login register cleared

--- src/login.js
+++ src/login.js
@@ -1,23 +1,41 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "./firebase";
 
 export default function Login() {
-  const login = () => {};
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // alert(e.target[0].value);
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (err) {
+      setErr(true);
+    }
+  };
 
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Hike</span>
         <span className="title">Login</span>
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
           <input className="input" type="email" placeholder="email" />
           <input className="input" type="password" placeholder="password" />
           <input id="file" type="file" style={{ display: "none" }} />
-          <button onClick={login}>Login</button>
+          <button>Login</button>
+          {err && <span>Something went wrong</span>}
         </form>
-        <Link to="/register">
-          <p>New user? Register</p>
-        </Link>
+        <p>
+          You don't have an account? <Link to="/register">Register</Link>
+        </p>
       </div>
     </div>
   );
