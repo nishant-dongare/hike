a645105a1aa6cb488791a7fa1a32d24034ffea59
nishant-dongare <84028631+nishant-dongare@users.noreply.github.com>
2023-01-04 19:28:42 +0530

    firebase removed

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const fbConfig = {
-  apiKey: "AIzaSyDeQsEK5Vb7mF1T99JL5vBW94rA7SCsUFQ",
-  authDomain: "hike-ebfbc.firebaseapp.com",
-  projectId: "hike-ebfbc",
-  storageBucket: "hike-ebfbc.appspot.com",
-  messagingSenderId: "109314709334",
-  appId: "1:109314709334:web:8bc7f892dbfc415334e8f7",
-  measurementId: "G-JGZXDHCVYL",
+  //Add your config
 };
 
 const app = getApps().length === 0 ? initializeApp(fbConfig) : getApp();
